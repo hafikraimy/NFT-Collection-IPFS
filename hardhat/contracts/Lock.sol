d6ff0dc14bfdffd72f1581a3389749beedc0e29c
Colin Stark <collinscodes@gmail.com>
2022-10-18 10:14:53 -0800

    Update Lock.sol
    Added comment for easy understanding of the project

--- hardhat/contracts/Lock.sol
+++ hardhat/contracts/Lock.sol
@@ -1,15 +1,29 @@
-// SPDX-License-Identifier: UNLICENSED
+/// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
+/// Import this file to use console.log
 import "hardhat/console.sol";
 
+
+/**
+@title A time lock Contract
+@author Hafikraimy
+*/
 contract Lock {
+
+    /**
+    @dev create global variable which will be called by functions for different purposes
+    */
+
     uint public unlockTime;
     address payable public owner;
-
     event Withdrawal(uint amount, uint when);
 
+    /**
+    @notice This function will run when this contract is deployed into the blockchain and will run only at that time
+    @dev The current time must be less than the unlocking time 
+    @param _unlockTime The time at which it will be unlocked
+    */
     constructor(uint _unlockTime) payable {
         require(
             block.timestamp < _unlockTime,
@@ -20,6 +34,9 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
+    /**
+    @dev emit the Withdrawal event after all the required statement have been satisfied
+    */
     function withdraw() public {
         // Uncomment this line to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
